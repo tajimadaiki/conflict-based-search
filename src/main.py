4ad67cd4985d7940355c8f760f93348a188d0fc3
Daiki_Tajima <t.dai.1215@gmail.com>
2022-10-12 13:56:28 +0900

    mapをファイルから読み込めるようにした

--- src/main.py
+++ src/main.py
@@ -4,24 +4,25 @@ from agent import Agent
 from planner import Planner
 from visualizer import Visualizer
 import random
+import map_reader
+
+start_time = time.time()
+grid_size_x, grid_size_y, static_obstacles, end_points = map_reader.load_file('./map/kvc.map')
+
+# grid_size_x = 21
+# grid_size_y = 6
+# static_obstacles = []
+# end_points = []
+# for x in range(grid_size_x):
+#     for y in range(grid_size_y):
+#         if x%2==1 and 1<=y<=4:
+#             static_obstacles.append((x, y))
+#         else:
+#             end_points.append((x, y))
 
 
 agent_num = 5
 agents = [Agent(i) for i in range(agent_num)]
-grid_size_x = 21
-grid_size_y = 6
-static_obstacles = []
-possible_zone = []
-for x in range(grid_size_x):
-    for y in range(grid_size_y):
-        if x%2==1 and 1<=y<=4:
-            static_obstacles.append((x, y))
-        else:
-            possible_zone.append((x, y))
-
-# print(random.sample(possible_zone, 1))
-
-start_time = time.time()
 planner = Planner(agents, grid_size_x, grid_size_y, static_obstacles)
 intermediate_time = time.time()
 print(f"create planner: {intermediate_time-start_time}")
@@ -30,13 +31,13 @@ print(f"create planner: {intermediate_time-start_time}")
 starts = {}
 goals = {}
 for agent in agents:
-    start_pos = random.choice(possible_zone)
+    start_pos = random.choice(end_points)
     starts[agent] = start_pos
-    possible_zone.remove(start_pos)
+    end_points.remove(start_pos)
     #
-    goal_pos = random.choice(possible_zone)
+    goal_pos = random.choice(end_points)
     goals[agent] = goal_pos
-    possible_zone.remove(goal_pos)
+    end_points.remove(goal_pos)
     #
 print(starts, goals)
 
