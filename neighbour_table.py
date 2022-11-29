20f2ec1fda4e00cb222c44b7b1179c3ec1cc51c2
Daiki Tajima <t.dai.1215@gmail.com>
2022-11-30 05:35:11 +0900

    change neighbour_table interface

--- neighbour_table.py
+++ neighbour_table.py
@@ -1,5 +1,6 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 import numpy as np
+import openpyxl
 
 
 class NeighbourTable:
@@ -8,35 +9,55 @@ class NeighbourTable:
     directions_k3 = [(0, 0), (1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
 
     def __init__(self,
-                 grid_size_x: int,
-                 grid_size_y: int,
-                 static_obstacles: List[Tuple[int, int]],
-                 k: int = 2):
-        # set 2^k neighborhood movement
-        directions = []
-        if k == 2: directions = self.directions_k2
-        if k == 3: directions = self.directions_k3
+                 excel_map_file):
+        # load map file
+        self.load_excel_map(excel_map_file)
         # create neighbours table
-        table = dict()
-        for i in range(grid_size_x):
-            for j in range(grid_size_y):
+        self.table: Dict[Tuple[int,int]: str] = dict()
+        for x in range(self.grid_size_x):
+            for y in range(self.grid_size_y):
+                directions = []
                 neighbours = []
+                value = self.map[x][y]
+                if len(value) == 1:
+                    directions = [(0, 0), (1, 0), (-1, 0), (0, 1), (0, -1)]
+                else:
+                    directions.append((0, 0))
+                    for c in value[1:]:
+                        if c == 'r': directions.append((-1, 0))
+                        if c == 'u': directions.append((0, -1))
+                        if c == 'l': directions.append((1, 0))
+                        if c == 'd': directions.append((0, 1))
+                
                 for dx, dy in directions:
-                    x = i + dx
-                    y = j + dy
-                    if 0 <= x < grid_size_x and \
-                            0 <= y < grid_size_y and \
-                            not ((x, y) in static_obstacles):
-                        neighbours.append(np.array([x, y]))
-                table[(i, j)] = np.array(neighbours)
-        self.table = table
+                    nx = x+ dx
+                    ny = y + dy
+                    if 0 <= nx < self.grid_size_x and 0 <= ny < self.grid_size_y:
+                        if not (self.map[nx][ny] == '@'):
+                            neighbours.append(np.array([nx, ny]))
+                self.table[(x, y)] = np.array(neighbours)             
 
     def neighbours(self, pos: np.ndarray) -> np.ndarray:
         return self.table[tuple(pos)]
 
+    def load_excel_map(self, map_file):
+        wb = openpyxl.load_workbook(map_file)
+        map_sh = wb['map']
+        self.map: List[List[str]] = []
+        self.grid_size_x = map_sh.max_row
+        self.grid_size_y = map_sh.max_column
+        for row in range(1, map_sh.max_row + 1):
+            map_row = []
+            for col in range(1, map_sh.max_column + 1):
+                map_row.append(map_sh.cell(row, col).value)
+            self.map.append(map_row)
 
 if __name__ == "__main__":
-    obstacles = [(3, 4), (5, 5), (9, 2)]
-    neighbour = NeighbourTable(10, 15, obstacles)
-    pos44 = np.array([4, 4])
-    print(neighbour.neighbours(pos44))
+    map_file = "./map/map.xlsx"
+    neighbour = NeighbourTable(map_file)
+    max_x = neighbour.grid_size_x - 1
+    max_y = neighbour.grid_size_y - 1
+    print(max_x, max_y)
+    print(len(neighbour.map))
+    pos = np.array([4, 16])
+    print(neighbour.neighbours(pos))
