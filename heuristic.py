ae977a1e2fa520f4b1ed011917c97f4b0e3e092d
Daiki Tajima <t.dai.1215@gmail.com>
2022-11-30 08:29:06 +0900

    edit dist value

--- heuristic.py
+++ heuristic.py
@@ -6,8 +6,8 @@ from neighbour_table import NeighbourTable
 class Heuristic:
 
     def __init__(self,
-                 map_file: str):
-        self.neighbour_table = NeighbourTable(map_file)
+                 excel_map_file: str):
+        self.neighbour_table = NeighbourTable(excel_map_file)
         self.grid_size_x = self.neighbour_table.grid_size_x
         self.grid_size_y = self.neighbour_table.grid_size_y
         self._warshall_floyd()
@@ -21,22 +21,22 @@ class Heuristic:
         return int(np.linalg.norm(current - goal, 2))
 
     def single_shortest_path(self, current: np.ndarray, goal: np.ndarray) -> int:
-        return self.shortest_distance[current[0]][current[1]][goal[0]][goal[1]]
+        return self.shortest_distance[self.converter(current)][self.converter(goal)]
+    
+    # reshape [grid_size_x, grid_size_y] -> [grid_size_x * grid_size_y, 1]
+    def converter(self, pos: np.ndarray) -> int:
+        return pos[0] * self.grid_size_y + pos[1]
+
+    # reshape [[grid_size_x * grid_size_y, 1] -> [grid_size_x, grid_size_y]
+    def inverter(self, id_: int) -> np.ndarray:
+        i = id_ // self.grid_size_y
+        j = id_ % self.grid_size_y
+        return np.array([i, j])
 
     def _warshall_floyd(self) -> np.ndarray:
-
-        # reshape [grid_size_x, grid_size_y] -> [grid_size_x * grid_size_y, 1]
-        def converter(pos: np.ndarray) -> int:
-            return pos[0] * self.grid_size_y + pos[1]
-
-        # reshape [[grid_size_x * grid_size_y, 1] -> [grid_size_x, grid_size_y]
-        def inverter(id_: int) -> np.ndarray:
-            i = id_ // self.grid_size_y
-            j = id_ % self.grid_size_y
-            return np.array([i, j])
         
         def is_obstacle(id_: int) -> bool:
-            pos = inverter(id_)
+            pos = self.inverter(id_)
             return self.neighbour_table.is_obstacle(pos)
 
         inf = 1001001001
@@ -50,9 +50,9 @@ class Heuristic:
         for i in range(self.grid_size_x):
             for j in range(self.grid_size_y):
                 pos = np.array([i, j])
-                node_id = converter(pos)
+                node_id = self.converter(pos)
                 for n_pos in self.neighbour_table.neighbours(pos):
-                    n_node_id = converter(n_pos)
+                    n_node_id = self.converter(n_pos)
                     distance[node_id][n_node_id] = 1
         # calculate warshall_floyd
         for k in range(node_num):
@@ -63,14 +63,7 @@ class Heuristic:
                     if is_obstacle(j): continue
                     distance[i][j] = min([distance[i][j], distance[i][k] + distance[k][j]])
 
-        self.shortest_distance = np.full((self.grid_size_x, self.grid_size_y, self.grid_size_x, self.grid_size_y), inf)
-        print(distance)
-
-        for i in range(node_num):
-            for j in range(node_num):
-                i_pos = inverter(i)
-                j_pos = inverter(j)
-                self.shortest_distance[i_pos[0]][i_pos[1]][j_pos[0]][j_pos[1]] = distance[i][j]
+        self.shortest_distance  = distance
 
 
 if __name__ == "__main__":
